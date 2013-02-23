96c327517a3e9581cdc3ec771d518327c7abbb13
Lawson Kurtz <lawson.kurtz@viget.com>
2013-02-23 14:22:32 -0500

    base rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkmate::Application.config.secret_token = '78bdcca623cbbd5a699b17e689629d49d79a9390d204a6735cb5e67c525d7f0092a3aa8de33ece35b8cfa63fc2978ee7688e62baafced648e2508249ce80be40'
