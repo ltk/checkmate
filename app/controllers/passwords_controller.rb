8b7100f882e79c5878fa2922655da7182d689114
Lawson Kurtz <lawson.kurtz@viget.com>
2013-02-24 00:04:12 -0500

    build password reset form

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -4,7 +4,7 @@ class PasswordsController < ApplicationController
 
   def create
     # TODO add email index to users table
-    user = User.find_by_email(params[:email])
+    user = User.find_by_email(params[:user][:email])
 
     if user
       user.set_password_reset_token
@@ -14,5 +14,15 @@ class PasswordsController < ApplicationController
       flash.now[:error] = "No user was found for the given email address"
       render :new
     end
+  end
+
+  def edit
+    @user = User.find_by_password_reset_token(params[:token]) if params[:token].present?
+
+    unless @user
+      redirect_to new_password_path, :alert => "Invalid reset token"
+    end
+
+    self.current_user = @user
   end
 end
--- spec/controllers/passwords_controller_spec.rb
+++ spec/controllers/passwords_controller_spec.rb
@@ -15,19 +15,45 @@ describe PasswordsController do
       let(:user) { FactoryGirl.create(:user) }
 
       it "sends an email" do
-        post :create, :email => user.email
+        post :create, :user => { :email => user.email }
         PasswordResetMailer.deliveries.last.to.should eql([user.email])
       end
 
       it "redirects to the root path" do
-        post :create, :email => user.email
+        post :create, :user => { :email => user.email }
         page.should redirect_to "/"
       end
 
       it "displays a flash message" do
-        post :create, :email => user.email
+        post :create, :user => { :email => user.email }
         flash[:notice].should == "Check your email for reset instructions"
       end
     end
-  end 
+
+    context "with an unkown email address" do
+      it "re-renders the reset form" do
+        post :create, :user => { :email => "unknown@email.address" }
+        page.should render_template(:new)
+      end
+    end
+  end
+
+  describe "#edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { user.set_password_reset_token }
+
+    context "with a valid reset token" do
+      it "should display the reset form" do
+        get :edit, :user_id => user.id, :token => user.password_reset_token
+        page.should render_template(:edit)
+      end
+    end
+
+    context "with an invalid reset token" do
+      it "should redirect to the reset request form" do
+        get :edit, :user_id => user.id, :token => 'invalid-token'
+        response.should redirect_to(new_password_path)
+      end
+    end
+  end
 end
